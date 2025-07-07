bd01e7f80cca5cc6ddc0065522bb1a34d0990e3d
KRATICOS <UTTI232030@UTVCO.EDU.MX>
2025-07-07 17:05:35 -0600

    Update usuarioController.js

--- app/controllers/usuarioController.js
+++ app/controllers/usuarioController.js
@@ -35,7 +35,7 @@ exports.createUser = async (req, res) => {
         }
 
         const imagenes = files.map(file => ({
-            url: `http://localhost:3001/uploads/${file.filename}`
+            url: `https://backend-materialsdispenser-production.up.railway.app/uploads/${file.filename}`
         }));
 
         const newUser = new Usuario({
