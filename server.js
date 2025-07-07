a45d8fac7c5306e0d030de0341d0a8808f4e50ef
KRATICOS <UTTI232030@UTVCO.EDU.MX>
2025-07-07 17:54:32 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const DB_URL = process.env.DB_URL;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 mongoose.connect(DB_URL)
   .then(() => {
