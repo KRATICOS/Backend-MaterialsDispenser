ff0de105c5abac54f9d48303be3a6235fab3b9a1
KRATICOS <UTTI232030@UTVCO.EDU.MX>
2025-07-08 12:20:27 -0600

    Update app.js

--- app.js
+++ app.js
@@ -6,15 +6,11 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(cors({
-  origin: [
-    'capacitor://localhost',
-    'http://localhost',
-    'https://backend-materialsdispenser-production.up.railway.app',
-    'https://tu-frontend-pwa.web.app'
-  ],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
+
 
 app.use(bodyParser.json({ limit: '20mb' }));
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
@@ -34,5 +30,11 @@ app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/items', itemsRoutes);
 app.use('/api/inventario', inventarioRoutes);
 app.use('/api/historial', historialRoutes);
+
+
+app.get('/', (req, res) => {
+  res.send('Backend funcionando 🎉');
+});
+
 
 module.exports = app;
